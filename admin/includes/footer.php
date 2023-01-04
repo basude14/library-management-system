e45e306ca463f1e804bf3eda2a2ff636302e444c
basude14 <basuderohith2001@gmail.com>
2023-01-04 12:54:15 +0530

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  
+                   <em>Designed by Rohith Basude</em>
+                </div>
+            </div>
+        </div>
+    </section>
